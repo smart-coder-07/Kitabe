45d886f5ed66d5e6f84142cd234c22025438572d
smart-coder-07 <atultiwarikhj@gmail.com>
2024-04-20 10:54:13 +0100

    sec

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import ContactForm from "./Contact/ContactForm";
 const App = () => {
   return (
     <>
-      <div className="dark:bg-slate-900 dark:text-white">
+      <div className="dark:bg-slate-90 dark:text-white">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Courses />} />

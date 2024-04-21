1a3849cf601c764d9c0b6b829a69ad6ff61eb830
smart-coder-07 <atultiwarikhj@gmail.com>
2024-04-21 18:09:08 +0100

    lldfdh

--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -10,7 +10,7 @@ const FreeBook = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://kitabe-api.vercel.app/book");
         console.log(res.data);
         setBook(res.data.filter((data) => data.category === "Free"));
       } catch (error) {

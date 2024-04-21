4a0fd834bb39efd1aa1c7db3cb8763dda96a6fd0
smart-coder-07 <atultiwarikhj@gmail.com>
2024-04-21 16:12:57 +0100

    final

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 //middleware
 app.use(cors({
-  origin: ["https://deploy-mern-1whq.vercel.app"],
+  origin: ["https://kitabe.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true
 }));
